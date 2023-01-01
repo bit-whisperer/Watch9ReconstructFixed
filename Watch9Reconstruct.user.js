b6d1e0e6fb3103f48d8ae6479849ec60cdf93e7b
bit-whisperer <technica348@gmail.com>
2023-01-01 15:46:08 -0500

    Update Watch9Reconstruct.user.js

--- Watch9Reconstruct.user.js
+++ Watch9Reconstruct.user.js
@@ -14,7 +14,7 @@
 
 const w9rOptions = {
     oldAutoplay: true,        // Classic autoplay renderer with "Up next" text
-    removeBloatButtons: false  // Removes "Clip", "Thanks", "Download", etc. Now set to false since this doesn't work and im too lazy to remove the code
+    removeBloatButtons: false  // Removes "Clip", "Thanks", "Download", etc. Now set to false since this doesn't work and im too lazy to remove or fix the code
 }
 
 /**
